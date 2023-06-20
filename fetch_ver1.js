67d274d530a8942134b8bfd57e15f8e3a891ce02
shortcakesweets <olleh2020@kaist.ac.kr>
2023-06-20 18:01:59 +0900

    Frame
    Add frame, and fetching code

---
+++ fetch_ver1.js
@@ -0,0 +1,64 @@
+var urlGhot = 'https://p.eagate.573.jp/game/gfdm/gitadora_fuzzup/p/playdata/skill.html?gtype=gf&stype=1'
+var urlGoth = 'https://p.eagate.573.jp/game/gfdm/gitadora_fuzzup/p/playdata/skill.html?gtype=gf&stype=0'
+var urlDhot = 'https://p.eagate.573.jp/game/gfdm/gitadora_fuzzup/p/playdata/skill.html?gtype=dm&stype=1'
+var urlDoth = 'https://p.eagate.573.jp/game/gfdm/gitadora_fuzzup/p/playdata/skill.html?gtype=dm&stype=0'
+
+function fetchData(target_url){
+	return new Promise((resolve, reject) => {
+		fetch(url = target_url)
+		.then(response => response.text())
+		.then(html => {
+			const parser = new DOMParser();
+			const doc = parser.parseFromString(html, 'text/html');
+			
+			const element = doc.querySelector("#contents > div.maincont > div:nth-child(4) > table");
+			resolve(element);
+		})
+		.catch(error => {
+			reject(error);
+		});
+	});
+};
+
+function parseDORA(page){
+	//var pageHot = (page.getElementById("skillHot").getElementsByTagName("tbody"))[0];
+	//var pageOth = (page.getElementById("skillOth").getElementsByTagName("tbody"))[0];
+	
+	fetchData(urlDhot)
+		.then(value => {
+			var body = value.getElementsByTagName('tbody');
+			var rows = body[0].getElementsByTagName('tr');
+			for(let i=0; i<rows.length; i++){
+				var row = rows[i];
+				var jacket = (row.getElementsByClassName("jacket"))[0];
+				var point = (row.getElementsByClassName("skill_cell"))[0];
+				var percent = (row.getElementsByClassName("achive_cell"))[0];
+				var level = (row.getElementsByClassName("diff_cell"))[0];
+				
+				
+				
+				console.log("jacket:", jacket);
+				console.log("point:", point);
+				console.log("percent:", percent);
+				console.log("level:", level);
+				break;
+			}
+		})
+		.catch(error => {console.error('error:', error)});
+	fetchData(urlDoth)
+		.then(value => {console.log(value); oth = value;})
+		.catch(error => {console.error('error:', error)});
+};
+
+function makePage(){
+	var newPage = window.open('', '_blank');
+	putFrame(newPage);
+	return newPage;
+}
+
+function putFrame(page){
+	page.document.write('');
+}
+
+parseDORA();
+//var newPage = makePage();
